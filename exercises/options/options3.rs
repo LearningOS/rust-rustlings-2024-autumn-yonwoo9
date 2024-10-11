33f76d365a4ed074400a54e53c5441b253ece9b7
yonwoo9 <yonwoo9@gmail.com>
2024-10-11 21:32:10 +0800

    feat:update

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
@@ -14,7 +12,12 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        // some ref
+        // 避免不必要的复制或克隆
+        // 允许在不获取所有权的情况下访问数据
+        // 可以用于不可复制（non-Copy）类型
+        // 在处理嵌套的Option或复杂结构时特别有用
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
     y; // Fix without deleting this line.
